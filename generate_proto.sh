afb4abaf1aa904206945839dda93842bda7e5e6a
qiujiangkun <qiujiangkun@foxmail.com>
2021-08-04 01:57:29 +0800

    bugfixes

--- generate_proto.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-BASE_PATH=$(dirname $(realpath $0))
-protoc --plugin=protoc-gen-grpc_java=$(which protoc-gen-grpc-java) -I=$BASE_PATH/src/main/proto --grpc_java_out=src/main/java --java_out=src/main/java $BASE_PATH/src/main/proto/*
--- src/main/java/org/bytecamp/program_repair/astor/utils/Writer.java
+++
@@ -1,8 +0,0 @@
-package org.bytecamp.program_repair.astor.utils;
-
-public interface Writer {
-    void write(String chunk);
-    default void writeln(String line) {
-        this.write(line + "\n");
-    }
-}
